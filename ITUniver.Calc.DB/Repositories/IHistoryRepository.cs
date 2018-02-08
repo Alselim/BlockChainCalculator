5cb90faed68950b8bc9f7a2bcecd40da71f86bec
Aleksandr Limonov <a-limonov@mail.ru>
2018-02-08 15:50:09 +0400

    fix

---
+++ ITUniver.Calc.DB/Repositories/IHistoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ITUniver.Calc.DB.Models;
+using System;
+
+namespace ITUniver.Calc.DB.Repositories
+{
+    public interface IHistoryRepository
+    {
+        IHistoryItem Find(long id);
+
+        void Save(IHistoryItem item);
+
+        void Delete(long id);
+    }
+}
